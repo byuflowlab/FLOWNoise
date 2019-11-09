914e149045c1ced16a867912b726cdf6e7b57954
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2019-11-08 18:01:59 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# FLOW Noise
+
+Tools for computational aeroacoustics (preprocessing, calculations, and
+postprocessing) developed at BYU's FLOW lab.
+
+# Files
+  * `docs/`: Documentation (open Jupyter notebooks with [nbviewer](https://nbviewer.jupyter.org/)).
+
+# Dependencies
+  * [`GeometricTools`](https://github.com/byuflowlab/GeometricTools.jl)
+  * Paraview  : Not needed, but examples call Paraview for visualization of VTK
+      outputs.
+
+# Related Projects
+  * [Rotor-on-rotor aeroacoustic interactions](https://github.com/byuflowlab/alvarezcritchfield2020-noise-prop-on-prop)
+  *
+  * [BPM code for turbine acoustics](https://github.com/byuflowlab/bpm-turbine-acoustics)
+
+
+# Authorship
+  * Authors   : Eduardo J Alvarez and Tyler Critchfield
+  * Email     : Edo.AlvarezR@gmail.com
+  * Created   : Nov 2019
+  * License   : MIT License
---
+++ src/FLOWNoise.jl
@@ -0,0 +1,29 @@
+"""
+Tools for computational aeroacoustics (preprocessing, calculations, and
+postprocessing) developed at BYU's FLOW lab.
+
+    # AUTHORSHIP
+      * Author    : Eduardo J. Alvarez and Tyler Critchfield
+      * Email     : Edo.AlvarezR@gmail.com
+      * Created   : Nov 2019
+      * License   : MIT
+"""
+module FLOWNoise
+
+# ------------ FLOW CODES ------------------------------------------------------
+# GeometricTools https://github.com/byuflowlab/GeometricTools.jl
+import GeometricTools
+gt = GeometricTools
+
+# ------------ GENERIC MODULES -------------------------------------------------
+
+# ------------ GLOBAL VARIABLES ------------------------------------------------
+module_path = splitdir(@__FILE__)[1]                # Path to this module
+
+# ------------ HEADERS ---------------------------------------------------------
+# Load headers
+for module_name in []
+    include("FLOWNoise_"*module_name*".jl")
+end
+
+end # END OF MODULE
